268ea24e392870a7166d31c72eb9483ae52fbb11
testcord-py <tristondup2008@gmail.com>
2026-04-13 17:10:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VERSION="3.3.35"
+SCRIPT_VERSION="3.3.39"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_auto/"
 # Installed launcher (replaces upstream remnawave_reverse / rr): this file + symlink in /usr/local/bin
